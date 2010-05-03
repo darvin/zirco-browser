e5cefa2da0a5a6321ebb92c89731bc6701e354a4
jerem <jerem@e747efee-954d-ea6f-70a2-271fa72c5eea>
2010-05-03 18:28:17 +0000

    Add copyright notice.

--- Zirco/src/org/zirco/events/EventController.java
+++ Zirco/src/org/zirco/events/EventController.java
@@ -1,9 +1,23 @@
+/*
+ * Zirco Browser for Android
+ * 
+ * Copyright (C) 2010 J. Devauchelle and contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 package org.zirco.events;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 
 public class EventController {
 	
